48a19b6ad77451959dbdfbd76dd0c4a207b96f52
Royce Calvin <royce20002006@yahoo.com>
2024-01-24 23:01:36 -0600

    finished

--- dynamic-array.js
+++ dynamic-array.js
@@ -1,19 +1,35 @@
 class DynamicArray {
 
   constructor(defaultSize=4) {
-
-    this.defaultSize = defaultSize;
+    this.length = 0;
+    this.capacity = defaultSize;
+    this.data = new Array(this.capacity);
+    // Your code here 
 
   }
 
   read(index) {
-
-    // Your code here 
+    return this.data[index];
+    
   }
 
   unshift(val) {
-
-    // Your code here 
+    let newArr = [val];
+    this.length = 1;
+    
+    for (let i = 0; i < this.capacity; i++) {
+      if (this.data[i] !== undefined) {
+        newArr[i + 1] = this.data[i];
+      this.length++
+      }
+      
+    }
+    
+    this.data = newArr;
+    return this.data;
+    
+    
+    
   }
 
 }
